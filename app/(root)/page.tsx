dff6961084e5634e582c2577e7ecd26919bf7198
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-27 11:48:29 +0600

    zustand state management package initialize

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/model";
 
 const SetupPage = () => {
     return (
       <div className="p">
         <h1>This is Home page</h1>
-        <UserButton afterSignOutUrl="/"/>
+       <Modal title="test" description="test description" isOpen onClose={() => {}}>
+          Children
+       </Modal>
       </div>
     );
   };
