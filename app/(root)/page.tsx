5f1d943c37f59ec925b3b9b1f25a27c3fdb1a58a
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-27 10:32:57 +0600

    clerk ui implementation done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+    return (
+      <div className="p">
+        <h1>This is Home page</h1>
+        <Button size="default" variant="outline">Click Here!</Button>
+      </div>
+    );
+  };
+  
+  export default HomePage;
